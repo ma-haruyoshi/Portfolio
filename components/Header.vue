3f0ab116759cd9d995d8bc5f976e93fc70a2bab3
ma-haruyoshi <hanatokei128@gmail.com>
2021-08-20 14:07:24 +0900

    アイコン読込速度改善対応
    必要なアイコンだけ読み込むように変更。

--- components/Header.vue
+++ components/Header.vue
@@ -8,34 +8,50 @@
       <a v-scroll-to="'#home'" href="#">
         <v-btn text large class="font-weight-regular">
           <v-icon left>
-            mdi-home
+            iconHome
           </v-icon>
         </v-btn>
       </a>
       <a v-scroll-to="'#about'" href="#">
         <v-btn text large class="font-weight-regular">
           <v-icon left>
-            mdi-account
+            iconAccount
           </v-icon>
         </v-btn>
       </a>
       <a v-scroll-to="'#skills'" href="#">
         <v-btn text large class="font-weight-regular">
           <v-icon left>
-            mdi-desktop-classic
+            mdiDesktopClassic
           </v-icon>
         </v-btn>
       </a>
       <a v-scroll-to="'#gallaly'" href="#">
         <v-btn text large class="font-weight-regular">
           <v-icon left>
-            mdi-camera
+            iconCamera
           </v-icon>
         </v-btn>
       </a>
     </v-app-bar>
   </div>
 </template>
+<script>
+import { mdiHome } from '@mdi/js'
+import { mdiAccount } from '@mdi/js'
+import { mdiDesktopClassic } from '@mdi/js'
+import { mdiCamera } from '@mdi/js'
+export default {
+  data () {
+    return {
+      iconHome: mdiHome,
+      iconAccount: mdiAccount,
+      iconDesktopClassic: mdiDesktopClassic,
+      iconCamera: mdiCamera
+    }
+  }
+}
+</script>
 <style>
 a {
   text-decoration: none;
